1c48494f48fa893b580426070a7c0327ac719dd7
Angelo Lombardo <Angelo.Lombardo@nike.com>
2018-07-18 23:20:02 +0200

    First commit

---
+++ ThermoDroid/ThermoDroid.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ThermoDroid.ino
+ Created:	7/18/2018 11:13:16 PM
+ Author:	Kingo
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
